86f065c69a0f008797b2d8658d001d521a4ceb5b
giginet <giginet.net@gmail.com>
2018-10-05 10:35:55 +0900

    Implement Awesome test suites

--- run_tests.rb
+++ run_tests.rb
@@ -5,11 +5,20 @@ require_relative './interp'
 
 $function_definitions = {}
 
-def exec(statement)
+def execute(statement)
   env = {}
   evaluate(minruby_parse(statement), env)
 end
 
+def assert_evaluation(statement)
+  expected = eval(statement)
+  execute(statement).must_equal expected
+end
+
 describe 'GiginyanRubyInteprter' do
-  it { exec("p(1 + 1)").must_equal 2 }
+  it { assert_evaluation("p(1 + 1)") }
+  it { assert_evaluation("p(4 - 3)") }
+  it { assert_evaluation("p(3 * 4)") }
+  it { assert_evaluation("p(11 % 6 / 2)") }
+  it { assert_evaluation("p(1 + 2 + 3 % 4 * 5 * 6 + 7 - 8 / 9)") }
 end
